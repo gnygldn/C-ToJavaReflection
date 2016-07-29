07302cbc1e02399490104f97db6fecbe510e30ff
Güney Gülden <gnygldn@hotmail.com>
2016-07-29 16:08:47 +0300

    Add files via upload

---
+++ ClassLibrary1/FieldWriterFixer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ReflectionTraining2;
+using NUnit.Framework;
+using FluentAssertions;
+using gny.FromCSharpToJava;
+using NUnit.Framework.Internal;
+
+namespace ReflectionTraining2Fixer
+{
+    [TestFixture]
+    public class FieldWriterFixer
+    {
+       
+        [Test]
+       /* public void FieldWriterChecker()
+        {
+            FieldWriter field = new FieldWriter(public; int? asd;);;
+
+
+        }*/
+    }
+}
